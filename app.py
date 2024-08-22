28f1e2d768bb8de9885b2f2455f70efbec81c38e
khushboodudeja <147654319+khushboodudeja@users.noreply.github.com>
2024-08-23 01:41:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import numpy as np # type: ignore
+import pandas as pd  # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import pandas_datareader as data # type: ignore
+from keras.models import load_model # type: ignore
+import yfinance as yf # type: ignore
+import streamlit as st # type: ignore
+
+
+
+start = '2010-1-1'
+end = '2019-12-31'
+
+st.title('Stock Trend Prediction')
+
+user_input = st.text_input('Enter Stock Ticker', 'AAPL')
+df = yf.download(user_input, start=start, end=end)
+
+# Describing Data
+st.subheader('Data from 2010 - 2019')
+if not df.empty:
+    st.write(df.describe())
+else:
+    st.write("No data available for the selected ticker.")
+
+#Visualisations
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize = (12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize = (12,6))
+plt.plot(ma100, 'r')
+plt.plot(ma200, 'g')
+plt.plot(df.Close, 'b')
+st.pyplot(fig)
+
+
+#splitting data into training and testing
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array = scaler.fit_transform(data_training)
+
+
+
+
+
+#load my model
+model = load_model('keras_model.h5')
+# testing part
+# Testing part
+past_100_days = data_training.tail(100)
+final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+# Prepare x_test and y_test arrays
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100: i])
+    y_test.append(input_data[i, 0])
+
+# Convert to numpy arrays
+x_test, y_test = np.array(x_test), np.array(y_test)
+
+# Make predictions
+y_predicted = model.predict(x_test)
+
+# Inverse transform the predicted and actual values
+scaler = scaler.scale_
+scale_factor = 1 / scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+# Final graph
+st.subheader('Prediction vs Original')
+fig2 = plt.figure(figsize=(12, 6))
+plt.plot(y_test, 'b', label='Original Price')
+plt.plot(y_predicted, 'r', label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
